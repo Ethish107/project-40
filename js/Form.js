010d38bd6d8cc6c01b845beff43297bebf7bb4c4
Ethish107 <73375266+Ethish107@users.noreply.github.com>
2021-03-23 01:10:07 -0400

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -16,11 +16,11 @@ class Form{
         this.title.position(500, 50);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
-        this.input.position(600,400);
+        this.input.position(650,400);
         this.input.style('width', '200px');
         this.input.style('height', '20px');
         this.input.style('background', 'lavender');
-        this.button.position(600,500);
+        this.button.position(650,500);
         this.button.style('width', '200px');
         this.button.style('height', '40px');
         this.button.style('background', 'lightpink');
